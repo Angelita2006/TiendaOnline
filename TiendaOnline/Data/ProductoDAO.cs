8bc76d604fe28f55c91ec4b9887062fcefc84b1b
angel <angel@DESKTOP-KSEA7CO>
2025-12-29 09:01:14 +0100

    Entrega-01 (v1.0)

---
+++ TiendaOnline/Data/ProductoDAO.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TiendaOnline.Data
+{
+    public class ProductoDAO
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoDAO(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Models.Producto>> GetAllProductos()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        internal async Task<List<Models.Producto>> GetFiltradoPorTexto(string texto)
+        {
+            return await _context.Productos.Where(a => a.Nombre.ToLower().Contains(texto.ToLower())).ToListAsync();
+        }
+
+        public async Task<Models.Producto?> GetProductoById(int id)
+        {
+            return await _context.Productos.FindAsync(id);
+        }
+
+        public async Task AddProducto(Models.Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProducto(Models.Producto producto)
+        {
+            _context.Productos.Update(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProducto(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
---
+++ TiendaOnline/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnline.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un nombre.")]
+        [StringLength(100)]
+        public String Nombre { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir una descripción.")]
+        [StringLength(500)]
+        public String Descripcion { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un precio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio no puede ser negativo.")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal Precio { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la cantidad de existencias.")]
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad de existencias no puede ser negativa.")]
+        public int Existencias { get; set; }
+        [StringLength(200)]
+        public String? ImagenUrl { get; set; }
+
+    }
+}
