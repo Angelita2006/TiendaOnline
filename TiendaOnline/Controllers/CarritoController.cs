3d6b00e4e1517e74cfdc730f874016b4e067634d
Angelita <Alumno@TI2-04>
2026-01-21 17:27:31 +0100

    Entrega-05 (v1.4)

---
+++ TiendaOnline/Controllers/CarritoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using System.Diagnostics.Eventing.Reader;
+using TiendaOnline.Data;
+using TiendaOnline.Helpers;
+using TiendaOnline.Models;
+
+namespace TiendaOnline.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private ProductoDAO _productoDAO;
+        private readonly ISession _session;
+
+        public CarritoController(ApplicationDbContext context)
+        {
+            _productoDAO = new ProductoDAO(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var elementos = CarritoHelper.ObtenerCarrito(_session);
+            return View(elementos);
+        }
+
+        //[HttpGet]
+        //[AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Agregar(int ProductoId)
+        {
+            var producto = await _productoDAO.GetProductoById(ProductoId);
+            if (producto != null)
+            {
+                var elementos = CarritoHelper.ObtenerCarrito(_session);
+                foreach (var item in elementos)
+                {
+                    if (item.ProductoId == producto.Id)
+                    {
+                        item.Cantidad += 1;
+                        CarritoHelper.GuardarCarrito(_session, elementos);
+                        break;
+                    }
+                }
+                var nuevoItem = new CarritoItem
+                {
+                    ProductoId = producto.Id,
+                    Nombre = producto.Nombre,
+                    Precio = producto.Precio,
+                    Cantidad = 1
+                };
+                elementos.Add(nuevoItem);
+                CarritoHelper.GuardarCarrito(_session, elementos);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Eliminar(int ProductoId)
+        {
+            var elementos = CarritoHelper.ObtenerCarrito(_session);
+            var itemAEliminar = elementos.FirstOrDefault(i => i.ProductoId == ProductoId);
+            if (itemAEliminar != null)
+            {
+                elementos.Remove(itemAEliminar);
+                CarritoHelper.GuardarCarrito(_session, elementos);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Vaciar()
+        {
+            CarritoHelper.VaciarCarrito(_session);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TiendaOnline/Helpers/CarritoHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using TiendaOnline.Models;
+
+namespace TiendaOnline.Helpers
+{
+    public class CarritoHelper
+    {
+        private const string SessionKey = "Carrito";
+
+        // Obtener carrito desde sesión
+        public static List<CarritoItem> ObtenerCarrito(ISession session)
+        {
+            var carritoJson = session.GetString(SessionKey);
+            return string.IsNullOrEmpty(carritoJson)
+                ? new List<CarritoItem>()
+                : JsonSerializer.Deserialize<List<CarritoItem>>(carritoJson);
+        }
+
+        // Guardar carrito en sesión
+        public static void GuardarCarrito(ISession session, List<CarritoItem> carrito)
+        {
+            session.SetString(SessionKey, JsonSerializer.Serialize(carrito));
+        }
+
+        // Vaciar carrito
+        public static void VaciarCarrito(ISession session)
+        {
+            session.Remove(SessionKey);
+        }
+    }
+}
