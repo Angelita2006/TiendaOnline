78f560783a410fda3065a84273b1554ae31c7358
Angelita <Alumno@TI2-04>
2026-01-26 15:46:47 +0100

    Entrega-08 (v1.9)

--- TiendaOnline/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TiendaOnline/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,16 @@ namespace TiendaOnline.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            public string Nombre { get; set; }
+
+            public string Apellidos { get; set; }
+
+            public string Telefono { get; set; }
+
+            public string Direccion { get; set; }
+
+            public string Poblacion { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
